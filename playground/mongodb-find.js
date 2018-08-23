6281187e502e60a0b63ccdebf72ba83b3719a4ce
Utsav <ukaran11@gmail.com>
2018-08-23 14:54:58 +0530

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b7e6927fdfdfd48757906c0')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch to todos", err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log("Unable to fetch todos", err);
+  // });
+
+  db.collection('Users').find({name: 'Utsav'}).count().then((count) => {
+    console.log(`Users count: ${count}`);
+  }, (err) => {
+    console.log('unable to fetch Users', err);
+
+});
+
+  // client.close();
+});
